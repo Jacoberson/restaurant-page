5c67486a21dd2e827ec5f1a5f5480254df32060f
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-15 20:15:14 -0500

    Update webpack config with htmlwebpackplugin and HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Mort's Macarons",
+            template: 'src/template.html'
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
