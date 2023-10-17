3679ee14ad02d86d61806770609fca85a0f97d3e
Jacoberson <c.jacob.moore92@gmail.com>
2023-10-17 16:58:41 -0500

    Make MenuItem component

---
+++ src/pages/menuPage/components/menuItem.js
@@ -0,0 +1,29 @@
+const menuItem = (name, price, image) => {
+    const card = document.createElement('div');
+    card.classList.add('menu-card');
+
+    const cardText = document.createElement('div');
+    cardText.classList.add('cardText');
+
+    const cardName = document.createElement('h3');
+    cardName.textContent = name;
+    cardName.classList.add('menu-item');
+
+    const cardPrice = document.createElement('p');
+    cardTitle.textContent = `$${price}`;
+    cardTitle.classList.add('menu-item');
+
+    cardText.appendChild(cardName);
+    cardText.appendChild(cardPrice);
+
+    const cardImage = new Image();
+    cardImage.src = image;
+    cardImage.classList.add('menu-item', 'menu-image');
+
+    card.appendChild(cardText);
+    card.appendChild(cardImage);
+
+    return card;
+}
+
+export default menuItem;
